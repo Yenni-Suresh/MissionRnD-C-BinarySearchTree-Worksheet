5882dd26c722ba61e74d1c5042af7ce544e19017
Yenni-Suresh <Yenni-Suresh@missionrnd.com>
2016-04-13 02:02:14 +0530

    Submit Commit

--- src/FixBST.cpp
+++ src/FixBST.cpp
@@ -24,14 +24,63 @@ Nodes 1 and 20 need to be fixed here .
 */
 #include <stdio.h>
 #include <stdlib.h>
-
-
 struct node{
 	struct node * left;
 	int data;
 	struct node *right;
 };
+struct node * location(struct node *root, int data)
+{
+	struct node *p = root, *q = NULL;
+	while (p)
+	{
+		q = p;
+		if (p->data == data)
+			return p;
+		if (p->data > data)
+			p = p->left;
+		else p = p->right;
+	}
+	return q;
+}
+void fix(struct node *p[])
+{
+	p[0]->data = p[0]->data + p[1]->data;
+	p[1]->data = p[0]->data - p[1]->data;
+	p[0]->data = p[0]->data - p[1]->data;
+}
+void find(struct node *t, struct node *p[], struct node *root)
+{
+	if (t->right && p[0] == NULL)
+	{
+		p[1] = location(root, t->right->data);
+		if (t->right == p[1])
+			find(t->right, p, root);
+		else{
+			   p[0] = t->right;
+			   if (root->data > root->right->data && root->left->data < root->right->data)
+				   p[1] = root;
+		   }
+	}
+	if (t == root && p[0] == NULL && t->data < t->left->data)
+	{
+		p[1] = location(root->left, root->data);
+		p[0] = root;
+	}
+   else if (t->left && p[0] == NULL)
+	{
+		p[1] = location(root, t->left->data);
+		if (t->left == p[1])
+			find(t->left, p, root);
+		else p[0] = t->left;
+	}
+}
 
 void fix_bst(struct node *root){
 
+	struct node *p[2] = { NULL, NULL };
+	if (root == NULL)
+		return;
+	find(root, p, root);	
+	fix(p);
 }
