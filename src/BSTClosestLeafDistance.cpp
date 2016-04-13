3206bc7eb2c6cba8e4a55c2a835ab43c7c8f9db6
Yenni-Suresh <Yenni-Suresh@missionrnd.com>
2016-04-13 14:19:16 +0530

    Submit Commit

--- src/BSTClosestLeafDistance.cpp
+++ src/BSTClosestLeafDistance.cpp
@@ -38,8 +38,64 @@ struct node{
   int data;
   struct node *right;
 };
-
+void closest(struct node *t,int start,int *distance)
+{
+	if (!t->left && !t->right)
+	{
+		if (*distance == -1)
+			*distance = start;
+		else if (start < *distance)
+			*distance = start;
+		return;
+	}
+	if (t->left)
+		closest(t->left,start+1,distance);
+	if (t->right)
+	    closest(t->right,start+1,distance);
+}
+int get_closest(struct node *t)
+{
+	int distance = -1;
+		if (t)
+			closest(t, 0, &distance);
+		else return -1;
+	return distance + 1;
+}
+void process(struct node *root, int data, int *dist)
+{
+	int v1, v2,v;
+	if (root->data == data)
+	{
+		v1 = get_closest(root->left);
+		v2 = get_closest(root->right);
+		if (v1 == -1 && v2 == -1)
+			v1 = 0;
+		else if (v1 == -1 || (v2 < v1 && v2 != -1))
+			v1 = v2;
+		if (*dist > v1)
+			*dist = v1;
+	}
+	else if (root->data < data)
+	{
+		v = get_closest(root->left) + 1;
+		if ((*dist == -1 || *dist > v)&&v!=0)
+			*dist = v;
+		process(root->right, data, dist);
+		
+	}
+	else{
+		v = get_closest(root->right);
+		if ((*dist == -1 || *dist > v) && v != 0)
+			*dist = v;
+		process(root->left, data, dist);
+	}
+	
+}
 int get_closest_leaf_distance(struct node *root, struct node *temp)
 {
-  return -1;
+	if (root == NULL || temp == NULL )
+		return -1;
+	int dist = -1;
+	process(root, temp->data, &dist);
+  return dist;
 }
